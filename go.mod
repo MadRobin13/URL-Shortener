edab341d0c0fcfce543773e8b7bbd636c9e03abe
MadRobin13 <madrobin1340@gmail.com>
2024-08-29 17:08:02 -0400

    added api and unit testing

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.22.5
 require (
 	github.com/gin-gonic/gin v1.10.0
 	github.com/go-redis/redis/v8 v8.11.5
+	github.com/stretchr/testify v1.9.0
 )
 
 require (
--- store/store_service.go
+++ store/store_service.go
@@ -35,3 +35,19 @@ func InitializeStore() *StorageService {
 	storeService.redisClient = redisClient
 	return storeService
 }
+
+func SaveUrlMapping(shortUrl string, originalUrl string, userId string){ 
+	err := storeService.redisClient.Set(ctx, shortUrl, originalUrl, CacheDuration).Err()
+	if err != nil {
+		panic(fmt.Sprintf("Failed saving key url | Error: %v - shortUrl: %v - originalUrl: %v", err, shortUrl, originalUrl))
+	}
+
+}
+
+func RetrieveInitialUrl(shortUrl string) string {
+	result, err := storeService.redisClient.Get(ctx, shortUrl).Result()
+	if err != nil {
+		panic(fmt.Sprintf("Coudl not retrieve url | Error: %v - shortUrl: %v", err, shortUrl))
+	}
+	return result
+}
