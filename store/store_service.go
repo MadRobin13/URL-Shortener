0ca5bc18ae32311ea9615cfad5ed97257ab5c316
MadRobin13 <madrobin1340@gmail.com>
2024-08-29 14:18:42 -0400

    added redis stuf and basic server creation

---
+++ store/store_service.go
@@ -0,0 +1,37 @@
+package store
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/go-redis/redis/v8"
+)
+
+type StorageService struct {
+	redisClient *redis.Client
+}
+
+var (
+	storeService = &StorageService{}
+	ctx = context.Background()
+)
+
+const CacheDuration = 6 * time.Hour
+
+func InitializeStore() *StorageService {
+	redisClient := redis.NewClient(&redis.Options{
+		Addr : "localhost:6379",
+		Password : "",
+		DB : 0,
+	})
+
+	pong, err := redisClient.Ping(ctx).Result()
+	if err != nil {
+		panic(fmt.Sprintf("Error init Redis: %v", err))
+	}
+
+	fmt.Printf("\nRedis started successfully: pong mesage = {%s}", pong)
+	storeService.redisClient = redisClient
+	return storeService
+}
